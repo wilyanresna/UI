a2b70dd0c0caebd95c0e809269f9dae1c0c05f9c
PandanW4ngi <wilyanresna@gmail.com>
2018-01-16 09:20:30 +0700

    Rubah struktur folder

--- app/src/main/java/com/example/agastya/standardui/view/activity/MainActivity.java
+++ app/src/main/java/com/example/agastya/standardui/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.agastya.standardui;
+package com.example.agastya.standardui.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.agastya.standardui.R;
 
 public class MainActivity extends AppCompatActivity {
 
